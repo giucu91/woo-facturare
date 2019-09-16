dc3b945dcd93ac266c60e2ed96fa02e41e5e5f4b
George Ciobanu <george.ciobanu91@gmail.com>
2019-09-17 00:43:20 +0300

    added pot file

--- Gruntfile.js
+++ Gruntfile.js
@@ -14,7 +14,7 @@ module.exports = function( grunt ) {
 		checktextdomain: {
 			standard: {
 				options: {
-					text_domain: [ 'woo-facturare' ], //Specify allowed domain(s)
+					text_domain: [ 'woo-facturare', 'woocommerce' ], //Specify allowed domain(s)
 					create_report_file: 'true',
 					keywords: [ //List keyword specifications
 						'__:1,2d',
